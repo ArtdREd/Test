cc17d7609ce0dfd025e3f0e3989264f83d9ffd25
artem <artem@DESKTOP-4GKFBN6>
2022-06-03 14:54:00 +0300

    +

--- TicTacToe/src/home/TicTacToe/ActionFigure.java
+++ TicTacToe/src/home/TicTacToe/ActionFigure.java
@@ -1,16 +1,16 @@
 package home.TicTacToe;
 
 public enum ActionFigure {
-	Krestik("  +  "),  Nolik("  O  ");
+ Krestik("  +  "), Nolik("  O  ");
 
-	private String name;
+ private String name;
 
-	private ActionFigure(String name) {
-		this.name = name;
-	}
+ private ActionFigure(String name) {
+  this.name = name;
+ }
 
-	public String getName() {
-		return name;
-	}
+ public String getName() {
+  return name;
+ }
 
 }
